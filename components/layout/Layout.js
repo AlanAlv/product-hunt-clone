916124ccee89b0299d666958acc117e73acdce5d
AlanAlv <alan.alv96@gmail.com>
2020-07-02 00:02:25 -0500

    Global Css

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,8 +1,44 @@
-import Link from 'next/link';
+import { Global, css} from '@emotion/core';
 import Header from './Header'
 const Layout = props => {
     return ( 
         <>
+        <Global 
+            styles={css`
+                :root {
+                    --gray: #3D3D3D;
+                    --gray2: #6F6F6F;
+                    --orange: #DA552F
+                }
+
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box
+                }
+                *, *:bwfoew, *:after {
+                    box-sizing: inherit;
+                }
+
+                body {
+                    font-size: 1.6rem;
+                    line-height: 1.5
+                }
+                h1, h2, h3 {
+                    margin: 0 0 2rem 0;
+                    line-height: 1.5;
+                }
+
+                ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                a {
+                    text-decoration: none
+                }
+            `}
+        />
             <Header />
             <main>
                 {props.children}
