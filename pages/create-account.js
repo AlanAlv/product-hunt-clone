f6f3846b11b8d7399f73c831dd3991882ea39531
AlanAlv <alan.alv96@gmail.com>
2020-07-02 14:23:00 -0500

    Validation Rules

--- pages/create-account.js
+++ pages/create-account.js
@@ -1,13 +1,29 @@
 import styled from '@emotion/styled';
 import Layout from '../components/layout/Layout';
 import { Form, Field, InputSubmit } from '../components/ui/Form';
+import useValidation from '../hooks/useValidation';
+import validateCreateAccount from '../validation/validateCreateAccount';
 
 const Heading = styled.h1`
   text-align: center;
   margin-top: 5rem;
 `;
 
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  password: ''
+}
+
 export default function CreateAccount() {
+
+
+  const {values, errors, submitForm, handleSubmit, handleChange} = useValidation(INITIAL_STATE, validateCreateAccount, createAccount);
+  
+  function createAccount () {
+    console.log('Creating acount...');
+  }
+  
   return (
 
     <>
---
+++ validation/validateCreateAccount.js
@@ -0,0 +1,24 @@
+export default function validateCreateAccount(values) {
+    let errors = {};
+
+    // Validate username
+    if(!values.name){
+        errors.name = "Name is required";
+    }
+
+    // Validate email
+    if(!values.email){
+        errors.email = "Email is required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "Invalid Email"
+    }
+
+    // Validate password
+    if(!values.password){
+        errors.password = "The Password is required";
+    } else if (values.password.length < 6){
+        errors.password = "Password must be at least 6 characters";
+    }
+
+    return errors;
+}
