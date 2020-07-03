a991111079b94b0e64ebf407c52f3e03c57aa295
AlanAlv <alan.alv96@gmail.com>
2020-07-03 00:09:25 -0500

    Firebase config

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCitezdM1_kiQQH1zHSVHDI8DUeluDkUiQ",
+    authDomain: "product-hunt-749c4.firebaseapp.com",
+    databaseURL: "https://product-hunt-749c4.firebaseio.com",
+    projectId: "product-hunt-749c4",
+    storageBucket: "product-hunt-749c4.appspot.com",
+    messagingSenderId: "171941175871",
+    appId: "1:171941175871:web:16b9e03547f4e906ec0e8a"
+};
+
+export default firebaseConfig;
