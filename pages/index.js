0865be13d5f33eaf895ddfd715a46e5b8651c2a9
AlanAlv <alan.alv96@gmail.com>
2020-07-03 19:56:33 -0500

    Get products query

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-import styled from '@emotion/styled'
-import Layout from '../components/layout/Layout'
+import React, { useEffect, useState, useContext } from 'react';
+import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 const Heading = styled.h1`
   color: red;
 `;
 
 export default function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handleSnapshot);
+    }
+
+    getProducts();
+  }, [])
+
+  function handleSnapshot(snapshot) {
+    const products = snapshot.docs.map(doc => {
+      return{
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    console.log(products)
+  }
+
   return (
 
     <div>
