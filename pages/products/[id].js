a8a02abaaab8f8618cb943fbc79e5b57ca0eec56
AlanAlv <alan.alv96@gmail.com>
2020-07-04 22:00:10 -0500

    Validate product exists

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,18 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
+import { FirebaseContext} from '../../firebase';
+import Layout from '../../components/layout/Layout';
+import Error404 from '../../components/layout/Error404';
 
 const Product = ({props}) => {
+
+    // Product state
+    const [product, setProduct] = useState({});
+    const [error, setError] = useState(false);
+
     const router = useRouter();
     const { query: { id } } = router;
 
+    // Firebase context 
+    const { firebase } = useContext(FirebaseContext);
 
     useEffect(() => {
         if(id){
-            console.log('ID: ', id);
+            const getProduct = async () => {
+                const productQuery = await firebase.db.collection('products').doc(id);
+                const product = await productQuery.get();
+                if(product.exists){
+                    setProduct(product.data());
+                } else {
+                    setError(true);
+                }
+            }
+            getProduct();
         }
     }, [id])
+
     return ( 
-        <h1>Product { id }</h1>
+        <Layout>
+            <>
+            { error && <Error404 /> }
+            </>
+        </Layout>
      );
 }
  
