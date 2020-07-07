445b1ea2449df35a98ea8dc53990d5976f57d203
AlanAlv <alan.alv96@gmail.com>
2020-07-07 00:51:56 -0500

    Redirect to Search Component

--- components/ui/Search.js
+++ components/ui/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
+import Router from 'next/router'
 
 const InputText = styled.input`
     border: 1px solid var(--gray3);
@@ -35,6 +36,12 @@ const Search = () => {
         e.preventDefault();
 
         if(search.trim() === '') return;
+
+        // Redirect user
+        Router.push({
+            pathname: '/search',
+            query: { q: search}
+        })
     }
     return ( 
         <form
--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled'
 import Layout from '../components/layout/Layout'
+import { useRouter } from 'next/router'
 
 const Heading = styled.h1`
   color: red;
 `;
 
 export default function Search() {
+  const router = useRouter();
+  const {query: {q} } = router;
+  console.log(q);
   return (
 
     <div>
