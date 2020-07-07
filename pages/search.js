5adc60556c0d2818daa81fecd5d3a54b69cba1f2
AlanAlv <alan.alv96@gmail.com>
2020-07-07 01:02:19 -0500

    Search objects

--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,56 @@
-import styled from '@emotion/styled'
-import Layout from '../components/layout/Layout'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
+import { useRouter } from 'next/router';
+import ProductDetails from '../components/layout/ProductDetails';
+import useProducts from '../hooks/useProducts';
+
 
 const Heading = styled.h1`
   color: red;
 `;
 
+
+
 export default function Search() {
+
+
   const router = useRouter();
   const {query: {q} } = router;
-  console.log(q);
+
+  const { products } = useProducts('created');
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const search = q.toLowerCase();
+    const filter = products.filter(product => {
+      return(
+        product.name.toLowerCase().includes(search) ||
+        product.description.toLowerCase().includes(search) ||
+        product.company.toLowerCase().includes(search)
+      );
+    })
+
+    setResult(filter);
+  }, [q, products]);
+
   return (
+
 
     <div>
       <Layout>
-        <Heading>Search</Heading>
+        <div className="list-products">
+          <div className="container">
+            <ul className="bg-white">
+              {result.map(product => (
+                <ProductDetails
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
 
     </div>
